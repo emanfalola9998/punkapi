0c8dcd675871f154fedc465288b72bdfc446a01e
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-04-30 10:36:09 +0100

    added initial test

--- src/components/NavMenu/NavItems/NavItems.test.tsx
+++ src/components/NavMenu/NavItems/NavItems.test.tsx
@@ -1,16 +1,27 @@
-// import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import NavItems from './NavItems'; // Assuming NavItems is in the same directory
-// import toggleNav from '../NavMenu'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NavItems from './NavItems'; // Assuming NavItems is in the same directory
+import { BrowserRouter } from 'react-router-dom';
 
-
-//     it('should render the menu icon', () => {
-//         // Arrange
-//         render(<NavItems toggleNav={toggleNav}/>);
-
-//         // Act
-//         const menuIcon = screen.getByAltText('Close menu');
-
-//         // Assert
-//         expect(menuIcon).toBeInTheDocument();
-//     });
+it('should render the menu icon',  () => {
+    // const toggleNav = jest.fn();
+    
+    // Arrange
+    render(
+    <BrowserRouter>
+        <NavItems 
+        toggleNav={() => console.log("nav toggled")}
+        />
+    </BrowserRouter>
+    ); 
+    
+    // Act
+    const menuIcon =  screen.getByAltText('Close menu');
+    
+    // Assert
+    expect(menuIcon).toBeInTheDocument();
+    
+    // Optional: Test if toggleNav is called when menu icon is clicked
+    // userEvent.click(menuIcon);
+    // expect(toggleNav).toHaveBeenCalled();
+});
