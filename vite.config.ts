a03352334b580de9970ad24f880db1ada9a8f45e
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2025-06-05 12:28:01 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
+// vite.config.ts
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // serve from root
+  base: "/punkapi/", // <-- important
   server: {
     proxy: {
       '/api': {
-        target: 'localhost:3001',
+        target: 'http://localhost:3001', // add http
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
 });
-
